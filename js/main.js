38a93a865b429f01c5e15d59fb1281520b0d2079
holy-baker <147312260+holy-baker@users.noreply.github.com>
2024-05-26 10:53:21 +0200

    20240526

--- js/main.js
+++ js/main.js
@@ -7,16 +7,6 @@ function myFunction() {
   }
 }
 
-// damit parallax-effect auch auf smartphones richtig dargestellt wird:
-const parallax = document.getElementById("parallax-1");
-window.addEventListener('scroll', function () {
-    // let offset = this.window.pageYOffset;
-    // elements.forEach((element) => {
-    // let offset = (element.offsetTop - (window.pageYOffset)) * 0.8
-    // element.style.backgroundPositionY = `${offset}px`;
-    // });
-});
-
 
 //  Kontaktformular: Senden-Button erst nach 10 Sekunden erlauben
 setTimeout (function(){
@@ -37,84 +27,3 @@ setTimeout (function(){
   }
 },1000);
 }
-
-
-
-//Kontaktformular: Datenverarbeitung bzw. übermittlung an Server
-
-    // absenden.addEventListener("click", function(event) {  
-    //   event.preventDefault();
-
-    //   let Name = document.getElementById("name");
-    //   console.log(Name.value);
-
-    //   let Jobtitel = document.getElementById("jobtitle");
-    //   console.log(Jobtitel.value);
-
-    //   let email = document.getElementById("email");
-    //   console.log(email.value);
-
-    //   let Telefonnummer = document.getElementById("phonenumber");
-    //   console.log(Telefonnummer.value);
-
-    //   let Nachricht = document.getElementById("message");
-    //   console.log(Nachricht.value);
-    //         //  console.log(event);
-
-    //   let data = {
-    //     Name: Name.value,
-    //     email: email.value,
-    //     Telefonnummer: Telefonnummer.value,
-    //     Nachricht: Nachricht.value
-    //   };
-
-    //   let url = "http://localhost:3000/";
-
-    //   let request = new Request(url, {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json"
-    //     },
-    //     body: JSON.stringify(data)
-    //   });
-
-    //   fetch(request)
-    //     .then((response) => response.json())
-    //     .then((data) => {
-    //       console.log("Antwort vom Server:", data);
-    //       //myForm.reset();
-    //     })
-    //     .catch((error) => {
-    //       console.warn(error);
-    //     });
-    //   });
-
-
-// Datenübermittlung via email mit hilfe eines email-services
-
-    // const requestUrl  = 'https://formsubmit.co/seppl1.1990@freenet.de'
-    //  function sendData() {
-    //    let formBody = [
-    //      'anrede' + encodeURIComponent(document.getElementById('anrede').value),
-    //      'vorname' + encodeURIComponent(document.getElementById('vorname').value),
-    //      'nachname' + encodeURIComponent(document.getElementById('nachname').value),
-    //      'jobtitel' + encodeURIComponent(document.getElementById('jobtitel').value),
-    //      'email' + encodeURIComponent(document.getElementById('email').value),
-    //      'message' + encodeURIComponent(document.getElementById('message').value),
-    //    ].join('&')
-    //    let xhr = new XMLHttpRequest()
-    //    // statt GET mit POST ersetzen?
-    //    xhr.open('POST', requestUrl)
-    //    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-    //    xhr.onload = function (){
-    //      if (xhr.status === 200){
-    //        // zur "Danke"-Seite
-    //        window.location = 'danke-für-deine-nachricht.html'
-    //        return
-    //      }
-    //      alert('An error occurred')
-    //    }
-    //    xhr.send(formBody)
-    //  }
-
-
